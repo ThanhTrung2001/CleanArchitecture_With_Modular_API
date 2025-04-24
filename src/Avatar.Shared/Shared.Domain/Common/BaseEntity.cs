33665992e24f17f5e509b8eae96d4374de5f6796
Thanh Trung <thanhtrungtran13@gmail.com>
2025-04-24 09:24:32 +0700

    init: folder structure

---
+++ src/Avatar.Shared/Shared.Domain/Common/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avatar.Shared.Shared.Domain.Common
+{
+    public class BaseEntity
+    {
+        //[Key]
+        public Guid ID { get; set; }
+        public DateTimeOffset CreatedDate { get; set; } = DateTimeOffset.Now;
+        public DateTimeOffset? UpdatedDate { get; set; }
+        public DateTimeOffset? DeletedDate { get; set; }
+        public bool IsDeleted { get; set; } = false;
+
+    }
+}
